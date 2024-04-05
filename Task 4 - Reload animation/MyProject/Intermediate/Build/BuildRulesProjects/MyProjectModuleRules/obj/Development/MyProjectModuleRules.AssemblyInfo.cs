e003489a9a2ad4b1900c95b838dd343f5a5574a7
Zera <zeranet.mail@gmail.com>
2024-04-05 22:36:43 +0200

    animations

--- Task 4 - Reload animation/MyProject/Intermediate/Build/BuildRulesProjects/MyProjectModuleRules/obj/Development/MyProjectModuleRules.AssemblyInfo.cs
+++ Task 4 - Reload animation/MyProject/Intermediate/Build/BuildRulesProjects/MyProjectModuleRules/obj/Development/MyProjectModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProjectModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4405c1fd5bfea28e0ecfefe7adc5fe515020450c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d01046cf6db1c5e4702284cf645bf255df00a785")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProjectModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProjectModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
